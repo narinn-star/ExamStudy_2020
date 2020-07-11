014222b17180bd61492598300bccd4f4c4605ac4
narinn-star <skfls2618@naver.com>
2020-07-11 22:49:23 +0900

    배열/foreach문

---
+++ 2020.07.03_06/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace _2020._07._03_06
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(23, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(290, 122);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "\r\n     100개짜리 배열을 만들고\r\n     반복문 for를 사용하여 각 원소에\r\n     1 부터 100 까지의 정수를 넣은 후\r\n\r\n  " +
+    "   foreach 반복문을 사용하여 한 줄에\r\n     10개씩의 숫자를 출력하는 프로그램   \r\n\r\n";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "출  력";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 231);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "결과가 표시되는 곳입니다!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "과제1_배열 출력하기";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 2020.07.03_06/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2020._07._03_06
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] Array01 = new int[100];
+            label2.Text = "";
+
+            for (int i = 0; i < Array01.Length; i++)
+            {
+                Array01[i] = i + 1;
+            }
+
+            foreach (int output in Array01)
+            { 
+                if (output % 10 == 1)
+                {
+                    label2.Text += "\n";
+                    label2.Text += output + " ";
+                }
+                else
+                {
+                    label2.Text += output + " ";
+                }
+                
+            }
+        }
+    }
+}
