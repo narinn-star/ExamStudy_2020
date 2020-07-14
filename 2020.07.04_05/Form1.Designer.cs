ea8fd4dee5a124c75777e12f0bac9dad1fa517bd
narinn-star <skfls2618@naver.com>
2020-07-14 23:56:35 +0900

    label에 image출력

---
+++ 2020.07.04_05/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace _2020._07._04_05
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "사진 하나",
+            "사진 둘",
+            "사진 셋",
+            "사진 넷",
+            "사진 다섯",
+            "사진 여섯"});
+            this.comboBox1.Location = new System.Drawing.Point(13, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Image = global::_2020._07._04_05.Properties.Resources.자동차_사진_1;
+            this.label1.Location = new System.Drawing.Point(66, 59);
+            this.label1.MinimumSize = new System.Drawing.Size(640, 480);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(640, 480);
+            this.label1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 605);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "7단원 혼자해보기 1번";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 2020.07.04_05/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2020._07._04_05
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+                label1.Image = Properties.Resources.자동차_사진_1;
+            else if (comboBox1.SelectedIndex == 1)
+                label1.Image = Properties.Resources.자동차_사진_2;
+            else if (comboBox1.SelectedIndex == 2)
+                label1.Image = Properties.Resources.자동차_사진_3;
+            else if (comboBox1.SelectedIndex == 3)
+                label1.Image = Properties.Resources.자동차_사진_4;
+            else if (comboBox1.SelectedIndex == 4)
+                label1.Image = Properties.Resources.자동차_사진_5;
+            else if (comboBox1.SelectedIndex == 5)
+                label1.Image = Properties.Resources.자동차_사진_6;
+        }
+    }
+}
