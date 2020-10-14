a17257c1a19b7df0fdcc9001f26d9ee4164f4771
narinn-star <skfls2618@naver.com>
2020-10-14 23:38:09 +0900

    20.10.14 REVIEW

--- Midterm[2020.2nd Semester]/중간실습시험2/실습문제1번(Validation).aspx
+++ Midterm[2020.2nd Semester]/중간실습시험2/실습문제1번(Validation).aspx
@@ -1,20 +1,189 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="실습문제1번(Validation).aspx.cs" Inherits="Midterm_2020._2nd_Semester_.중간실습시험2.실습문제1번_Validation_" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <style type="text/css">
+
+    .auto-style8 {
+        width: 977px;
+        height: 348px;
+    }
+    .auto-style18 {
+        text-align: center;
+    }
+    .auto-style25 {
+        width: 207px;
+    }
+    .auto-style26 {
+        width: 207px;
+        text-align: right;
+    }
+        .auto-style29 {
+            width: 148px;
+            text-align: right;
+        }
+        .auto-style30 {
+            width: 148px;
+        }
+        .auto-style31 {
+            width: 328px;
+        }
+        .auto-style33 {
+            color: #FF0000;
+        }
+        .auto-style34 {
+            width: 165px;
+        }
+    </style>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
     <p>
+        &nbsp;<table class="auto-style8">
+        <tr>
+            <td class="auto-style18" colspan="2">회원가입양식</td>
+            <td class="auto-style30">&nbsp;</td>
+            <td class="auto-style31">&nbsp;</td>
+        </tr>
+        <tr>
+            <td class="auto-style26">로그인정보</td>
+            <td class="auto-style34">&nbsp;</td>
+            <td class="auto-style29">
+                <asp:Label ID="Label1" runat="server"></asp:Label>
+            </td>
+            <td class="auto-style31">&nbsp;</td>
+        </tr>
+        <tr>
+            <td class="auto-style26">사용자 ID : </td>
+            <td class="auto-style34">
+                <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            </td>
+            <td class="auto-style29">
+                <asp:Label ID="Label2" runat="server"></asp:Label>
+            </td>
+            <td class="auto-style31">
+                <asp:Label ID="Label12" runat="server"></asp:Label>
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="TextBox1" CssClass="auto-style33" EnableClientScript="False" ErrorMessage="ID를 입력해주세요!"></asp:RequiredFieldValidator>
+            </td>
+        </tr>
+        <tr>
+            <td class="auto-style26">암호 : </td>
+            <td class="auto-style34">
+                <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            </td>
+            <td class="auto-style29">
+                <asp:Label ID="Label3" runat="server"></asp:Label>
+            </td>
+            <td class="auto-style31">
+                <asp:Label ID="Label13" runat="server"></asp:Label>
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ControlToValidate="TextBox2" CssClass="auto-style33" EnableClientScript="False" ErrorMessage="암호를 입력해주세요!"></asp:RequiredFieldValidator>
+            </td>
+        </tr>
+        <tr>
+            <td class="auto-style26">암호 확인 : </td>
+            <td class="auto-style34">
+                <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+            </td>
+            <td class="auto-style29">
+                <asp:Label ID="Label4" runat="server"></asp:Label>
+            </td>
+            <td class="auto-style31">
+                <asp:Label ID="Label14" runat="server"></asp:Label>
+                <asp:CompareValidator ID="CompareValidator1" runat="server" ControlToCompare="TextBox3" ControlToValidate="TextBox2" CssClass="auto-style33" EnableClientScript="False" ErrorMessage="암호가 일치하지 않습니다!" Visible="False"></asp:CompareValidator>
+            </td>
+        </tr>
+        <tr>
+            <td class="auto-style25">개인정보 (선택사항)</td>
+            <td class="auto-style34">&nbsp;</td>
+            <td class="auto-style30">
+                <asp:Label ID="Label5" runat="server"></asp:Label>
+            </td>
+            <td class="auto-style31">&nbsp;</td>
+        </tr>
+        <tr>
+            <td class="auto-style26">이름 (한글) : </td>
+            <td class="auto-style34">
+                <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+            </td>
+            <td class="auto-style29">
+                <asp:Label ID="Label6" runat="server"></asp:Label>
+            </td>
+            <td class="auto-style31">
+                <asp:Label ID="Label15" runat="server"></asp:Label>
+            </td>
+        </tr>
+        <tr>
+            <td class="auto-style26">이름 (영문) : </td>
+            <td class="auto-style34">
+                <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+            </td>
+            <td class="auto-style29">
+                <asp:Label ID="Label7" runat="server"></asp:Label>
+            </td>
+            <td class="auto-style31">
+                <asp:Label ID="Label16" runat="server"></asp:Label>
+            </td>
+        </tr>
+        <tr>
+            <td class="auto-style26">상세주소 : </td>
+            <td class="auto-style34">
+                <asp:TextBox ID="TextBox6" runat="server"></asp:TextBox>
+            </td>
+            <td class="auto-style29">
+                <asp:Label ID="Label8" runat="server"></asp:Label>
+            </td>
+            <td class="auto-style31">
+                <asp:Label ID="Label17" runat="server"></asp:Label>
+            </td>
+        </tr>
+        <tr>
+            <td class="auto-style26">시/도 : </td>
+            <td class="auto-style34">
+                <asp:TextBox ID="TextBox7" runat="server"></asp:TextBox>
+            </td>
+            <td class="auto-style29">
+                <asp:Label ID="Label9" runat="server"></asp:Label>
+            </td>
+            <td class="auto-style31">
+                <asp:Label ID="Label18" runat="server"></asp:Label>
+            </td>
+        </tr>
+        <tr>
+            <td class="auto-style26">우편번호 : </td>
+            <td class="auto-style34">
+                <asp:TextBox ID="TextBox8" runat="server"></asp:TextBox>
+            </td>
+            <td class="auto-style29">
+                <asp:Label ID="Label10" runat="server"></asp:Label>
+            </td>
+            <td class="auto-style31">
+                <asp:Label ID="Label19" runat="server"></asp:Label>
+                <asp:RegularExpressionValidator ID="RegularExpressionValidator2" runat="server" ControlToValidate="TextBox8" CssClass="auto-style33" EnableClientScript="False" ErrorMessage="올바르지 않은 우편번호" ValidationExpression="(D-)?\d{5}"></asp:RegularExpressionValidator>
+            </td>
+        </tr>
+        <tr>
+            <td class="auto-style26">전화번호 : </td>
+            <td class="auto-style34">
+                <asp:TextBox ID="TextBox9" runat="server"></asp:TextBox>
+            </td>
+            <td class="auto-style29">
+                <asp:Label ID="Label11" runat="server"></asp:Label>
+            </td>
+            <td class="auto-style31">
+                <asp:Label ID="Label20" runat="server"></asp:Label>
+                <asp:RegularExpressionValidator ID="RegularExpressionValidator3" runat="server" ControlToValidate="TextBox9" CssClass="auto-style33" EnableClientScript="False" ErrorMessage="올바르지 않은 전화번호" ValidationExpression="01(0|1|6|7|8|9)-\d{3,4}-\d{4}"></asp:RegularExpressionValidator>
+            </td>
+        </tr>
+        <tr>
+            <td class="auto-style25">&nbsp;</td>
+            <td class="auto-style34">
+                <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="비우기" />
+&nbsp;&nbsp;
+                <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="확인" />
+            </td>
+            <td class="auto-style30">&nbsp;</td>
+            <td class="auto-style31">&nbsp;</td>
+        </tr>
+    </table>
+    </p>
+    <p>
         <br />
     </p>
-    <p>
-    </p>
-    <p>
-    </p>
-    <p>
-    </p>
-    <p>
-    </p>
-    <p>
-    </p>
-    <p>
-    </p>
-</asp:Content>
+    </asp:Content>
--- Midterm[2020.2nd Semester]/중간실습시험2/실습문제1번(Validation).aspx.designer.cs
+++ Midterm[2020.2nd Semester]/중간실습시험2/실습문제1번(Validation).aspx.designer.cs
@@ -13,5 +13,329 @@ namespace Midterm_2020._2nd_Semester_.중간실습시험2
 
     public partial class 실습문제1번_Validation_
     {
+
+        /// <summary>
+        /// Label1 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// TextBox1 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+
+        /// <summary>
+        /// Label2 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
+        /// Label12 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label12;
+
+        /// <summary>
+        /// RequiredFieldValidator1 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
+
+        /// <summary>
+        /// TextBox2 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+
+        /// <summary>
+        /// Label3 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+
+        /// <summary>
+        /// Label13 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label13;
+
+        /// <summary>
+        /// RequiredFieldValidator2 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator2;
+
+        /// <summary>
+        /// TextBox3 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox3;
+
+        /// <summary>
+        /// Label4 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+
+        /// <summary>
+        /// Label14 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label14;
+
+        /// <summary>
+        /// CompareValidator1 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CompareValidator CompareValidator1;
+
+        /// <summary>
+        /// Label5 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label5;
+
+        /// <summary>
+        /// TextBox4 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox4;
+
+        /// <summary>
+        /// Label6 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label6;
+
+        /// <summary>
+        /// Label15 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label15;
+
+        /// <summary>
+        /// TextBox5 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox5;
+
+        /// <summary>
+        /// Label7 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label7;
+
+        /// <summary>
+        /// Label16 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label16;
+
+        /// <summary>
+        /// TextBox6 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox6;
+
+        /// <summary>
+        /// Label8 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label8;
+
+        /// <summary>
+        /// Label17 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label17;
+
+        /// <summary>
+        /// TextBox7 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox7;
+
+        /// <summary>
+        /// Label9 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label9;
+
+        /// <summary>
+        /// Label18 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label18;
+
+        /// <summary>
+        /// TextBox8 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox8;
+
+        /// <summary>
+        /// Label10 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label10;
+
+        /// <summary>
+        /// Label19 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label19;
+
+        /// <summary>
+        /// RegularExpressionValidator2 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator RegularExpressionValidator2;
+
+        /// <summary>
+        /// TextBox9 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox9;
+
+        /// <summary>
+        /// Label11 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label11;
+
+        /// <summary>
+        /// Label20 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label20;
+
+        /// <summary>
+        /// RegularExpressionValidator3 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator RegularExpressionValidator3;
+
+        /// <summary>
+        /// Button1 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+
+        /// <summary>
+        /// Button2 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button2;
     }
 }
